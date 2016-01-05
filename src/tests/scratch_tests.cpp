658c80ff7b75589607ee60c2ad3422c644f99df2
Graham MacDonald <grahamamacdonald@gmail.com>
2016-01-05 21:04:13 +0000

    vector class tests

--- src/tests/scratch_tests.cpp
+++ src/tests/scratch_tests.cpp
@@ -4,6 +4,9 @@
 
 TEST_CASE("Scratch", "[scratch]")
 {
+    CHECK(__alignof__(Vector) == 16);
+    CHECK(sizeof(Vector) == 16);
+
     CHECK(__alignof__(Point) == 16);
     CHECK(sizeof(Point) == 16);
 }
