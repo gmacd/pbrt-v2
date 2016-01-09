c66c793b48e4bcfe022d751132575800f7be4137
Graham MacDonald <grahamamacdonald@gmail.com>
2016-01-09 17:27:38 +0000

    Add tests, avx2.

--- src/tests/alignment_tests.cpp
+++ src/tests/alignment_tests.cpp
@@ -4,14 +4,14 @@
 
 TEST_CASE("Alignment", "[alignment]")
 {
-    CHECK(__alignof__(Vector) == 16);
+    CHECK(alignof(Vector) == 16);
     CHECK(sizeof(Vector) == 16);
 
-    CHECK(__alignof__(Point) == 16);
+    CHECK(alignof(Point) == 16);
     CHECK(sizeof(Point) == 16);
     
-    CHECK(__alignof__(Matrix4x4) == 16);
+    CHECK(alignof(Matrix4x4) == 16);
     CHECK(sizeof(Matrix4x4) == 64);
 
-    CHECK(__alignof__(Transform) == 16);
+    CHECK(alignof(Transform) == 16);
 }
